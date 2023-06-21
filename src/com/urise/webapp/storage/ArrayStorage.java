ca7799670b36115e84e802621c73b23b556b1283
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-06-21 15:26:56 +0300

    update

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,50 +1,12 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.model.Resume;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
-
-    public void update(Resume resume) {
-        int index = findIndex(resume.uuid);
-        if (index == -1) {
-            System.out.println("Resume with " + resume.uuid + " does not exist");
-        } else {
-            storage[index] = resume;
-            System.out.println("Update " + resume.uuid + " is correct");
-        }
-    }
-
-    public void save(Resume resume) {
-        int index = findIndex(resume.uuid);
-        if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage is full");
-        } else if (index != -1) {
-            System.out.println("Resume " + resume.uuid + " already exists");
-        } else {
-            storage[size] = resume;
-            size++;
-            System.out.println("Add " + resume.uuid + " in storage");
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = findIndex(uuid);
-        if (index == -1) {
-            System.out.println("Resume with " + uuid + " does not exist");
-            return;
-        }
-        storage[index] = storage[size - 1];
-        storage[size - 1] = null;
-        size--;
-    }
-
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-
 
     protected int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
