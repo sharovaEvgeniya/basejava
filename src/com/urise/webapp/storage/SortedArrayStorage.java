0f8493ca9c8ad28849b190a6f534489ad3770485
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-06-22 19:29:39 +0300

    update

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -9,6 +9,18 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     protected int findIndex(String uuid) {
         Resume searchRey = new Resume();
         searchRey.setUuid(uuid);
-        return Arrays.binarySearch(storage,0,size,searchRey);
+        return Arrays.binarySearch(storage, 0, size, searchRey);
+    }
+
+    @Override
+    protected void saveResume(int index, Resume resume) {
+        int insertionIndex = -findIndex(resume.uuid) - 1;
+        System.arraycopy(storage, insertionIndex, storage, insertionIndex + 1, size - insertionIndex);
+        storage[insertionIndex] = resume;
+    }
+
+    @Override
+    protected void deleteResume(int index) {
+        System.arraycopy(storage, index + 1, storage, index, size - index - 1);
     }
 }
