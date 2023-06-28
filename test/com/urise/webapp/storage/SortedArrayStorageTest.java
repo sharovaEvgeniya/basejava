fa5d2b5d90304f1b6dbc93b71b598febf2674ce1
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-06-28 17:36:22 +0300

    add tests

---
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,20 @@
+package com.urise.webapp.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortedArrayStorageTest {
+
+    @Test
+    public void findIndex() throws Exception {
+    }
+
+    @Test
+    public void saveResume() throws Exception {
+    }
+
+    @Test
+    public void deleteResume() throws Exception {
+    }
+}
