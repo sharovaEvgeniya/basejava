091ede298d20a5c5e69ecce55e9213553024e4ff
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-06-28 17:39:43 +0300

    add reflection

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -1,4 +1,18 @@
 package com.urise.webapp;
 
+import com.urise.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+
 public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException {
+        Resume resume = new Resume();
+        Field field = resume.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(resume));
+        field.set(resume, "new_uuid");
+        System.out.println(resume);
+        //TODO :  invoke resume.toString via reflection
+    }
 }
