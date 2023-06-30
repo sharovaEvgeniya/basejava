a376ce793dadc595295a0872a6b1d5ca16299b0f
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-06-30 17:09:13 +0300

    update

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,9 +3,11 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume resume = new Resume();
         Field field = resume.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -13,6 +15,10 @@ public class MainReflection {
         System.out.println(field.get(resume));
         field.set(resume, "new_uuid");
         System.out.println(resume);
-        //TODO :  invoke resume.toString via reflection
+
+        Class<? extends Resume> resumeReflection = resume.getClass();
+        Method method = resumeReflection.getMethod("toString");
+        Object obj = method.invoke(resume);
+        System.out.println(obj);
     }
 }
