9f9e23db1f60e58bf417c5e7b38fb33daf4b5289
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-08-11 16:15:56 +0300

    add suiteClass

---
+++ test/com/urise/webapp/storage/AllTestSuite.java
@@ -0,0 +1,16 @@
+package com.urise.webapp.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        ListStorageTest.class,
+        MapResumeStorageTest.class,
+        MapUuidStorageTest.class,
+        SortedArrayStorageTest.class
+})
+
+public class AllTestSuite {
+}
