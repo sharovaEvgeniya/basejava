0842b6bae039b6e9aec6bad081a8876ea9d79f18
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-08-16 19:03:38 +0300

    update constructor

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -1,25 +1,24 @@
 package com.urise.webapp.model;
 
+import java.util.Objects;
 import java.util.UUID;
 
 /**
  * Initial resume class
  */
-public class Resume {
+public class Resume implements Comparable<Resume> {
 
     // Unique identifier
     private final String uuid;
     private final String fullName;
-
-    public Resume() {
-        this(UUID.randomUUID().toString());
-    }
 
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
     }
 
     public Resume(String uuid, String fullName) {
+        Objects.requireNonNull(uuid, "uuid must not be null");
+        Objects.requireNonNull(fullName, "fullName must not be null");
         this.uuid = uuid;
         this.fullName = fullName;
     }
@@ -56,5 +55,11 @@ public class Resume {
                 "uuid='" + uuid + '\'' +
                 ", fullName='" + fullName + '\'' +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Resume o) {
+        int cmp = fullName.compareTo(o.fullName);
+        return cmp != 0 ? cmp : uuid.compareTo(o.uuid);
     }
 }
