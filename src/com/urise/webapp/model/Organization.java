aef0c3be093739bd9b58fdd763073eb0fff6b95e
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-08-31 16:33:14 +0300

    update

--- src/com/urise/webapp/model/Organization.java
+++ src/com/urise/webapp/model/Organization.java
@@ -3,30 +3,10 @@ package com.urise.webapp.model;
 import java.util.List;
 import java.util.Objects;
 
-public class Organization {
-    private String title;
-    private String website;
-    private List<Period> periods;
-
-
-    public Organization(String title, String website, List<Period> periods) {
+public record Organization(String title, String website, List<Period> periods) {
+    public Organization {
         Objects.requireNonNull(title, "Title must not be null");
         Objects.requireNonNull(website, "Website must not be null");
         Objects.requireNonNull(periods, "Periods must not be null");
-        this.title = title;
-        this.website = website;
-        this.periods = periods;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public List<Period> getPeriods() {
-        return periods;
     }
 }
--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -25,6 +25,8 @@ public class Resume implements Comparable<Resume> {
     }
 
     public Resume(String uuid, String fullName, Map<ContactType, String> contacts, Map<SectionType, Section> sections) {
+        Objects.requireNonNull(uuid, "uuid must not be null");
+        Objects.requireNonNull(fullName, "fullName must not be null");
         this.uuid = uuid;
         this.fullName = fullName;
         this.contacts = contacts;
