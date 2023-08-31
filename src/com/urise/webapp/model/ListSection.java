19d6bc618207bb9ab74e939d8d2e7aad259584ed
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-08-31 16:30:35 +0300

    update

--- src/com/urise/webapp/model/ListSection.java
+++ src/com/urise/webapp/model/ListSection.java
@@ -23,4 +23,15 @@ public class ListSection extends Section {
         return Objects.equals(strings, that.strings);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(strings);
+    }
+
+    @Override
+    public String toString() {
+        return "ListSection{" +
+                "strings=" + strings +
+                '}';
+    }
 }
