e9adb376a2a1c63546f8dbb3cff861d52844fe54
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-08-31 16:23:03 +0300

    update

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -25,6 +25,10 @@ public class Resume implements Comparable<Resume> {
     }
 
     public Resume(String uuid, String fullName, Map<ContactType, String> contacts, Map<SectionType, Section> sections) {
+        Objects.requireNonNull(uuid, "uuid must not be null");
+        Objects.requireNonNull(fullName, "fullName must not be null");
+        Objects.requireNonNull(contacts, "Contacts must not be null");
+        Objects.requireNonNull(sections, "Section must not be null");
         this.uuid = uuid;
         this.fullName = fullName;
         this.contacts = contacts;
