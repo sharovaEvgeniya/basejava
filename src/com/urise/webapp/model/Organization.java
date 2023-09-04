6d63ee3c9be1887268068b54a9f459dd4d63808b
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-09-04 18:41:03 +0300

    delete @safeVarargs

--- src/com/urise/webapp/model/Organization.java
+++ src/com/urise/webapp/model/Organization.java
@@ -3,8 +3,8 @@ package com.urise.webapp.model;
 import java.util.List;
 import java.util.Objects;
 
-public record Organization(String title, String website, List<Period> ... periods) {
-    @SafeVarargs
+public record Organization(String title, String website, List<Period> periods) {
+
     public Organization {
         Objects.requireNonNull(title, "Title must not be null");
         Objects.requireNonNull(website, "Website must not be null");
