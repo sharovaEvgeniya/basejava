82816628eefbc2766fbdd7a503b93a1487406549
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-09-12 17:14:52 +0300

    add

---
+++ src/com/urise/webapp/storage/ObjectStreamStorage.java
@@ -0,0 +1,23 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.io.*;
+
+public class ObjectStreamStorage extends AbstractFileStorage {
+    public ObjectStreamStorage(File directory) {
+        super(directory);
+    }
+
+    @Override
+    protected void doWrite(Resume resume, OutputStream outputStream) throws IOException {
+        try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)) {
+            objectOutputStream.writeObject(resume);
+        }
+    }
+
+    @Override
+    protected Resume doRead(InputStream inputStream) throws IOException {
+        return null;
+    }
+}
