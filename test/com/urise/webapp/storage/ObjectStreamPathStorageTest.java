910628902b03166a43f2bf16ed8b9d42dddfbd61
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-09-22 14:43:52 +0300

    22.09.23 HW_9

--- test/com/urise/webapp/storage/ObjectStreamPathStorageTest.java
+++ test/com/urise/webapp/storage/ObjectStreamPathStorageTest.java
@@ -1,5 +1,7 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.storage.strategy.ObjectStreamSerialize;
+
 public class ObjectStreamPathStorageTest extends AbstractStorageTest {
     public ObjectStreamPathStorageTest() {
         super(new PathStorage(new ObjectStreamSerialize(), STORAGE_DIR.getAbsolutePath()));
