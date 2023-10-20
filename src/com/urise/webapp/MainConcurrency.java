f3413ab96493f00aa8f8866c4407469c68fc9351
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-10-20 18:29:50 +0300

    20.10.23 HW_11

---
+++ src/com/urise/webapp/MainConcurrency.java
@@ -0,0 +1,33 @@
+package com.urise.webapp;
+
+public class MainConcurrency {
+    private static int counter;
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+            }
+        };
+        thread0.start();
+
+        new Thread(() -> System.out.println(Thread.currentThread().getName()
+                + ", " + Thread.currentThread().getState())).start();
+
+        System.out.println(thread0.getState());
+
+        for(int i =0; i < 10000; i++) {
+            new Thread(() -> {
+                for(int j = 0; j < 100; j++) {
+                    inc();
+                }
+            }).start();
+        }
+        Thread.sleep(500);
+        System.out.println(counter);
+    }
+    private static synchronized void inc() {
+        counter++;
+    }
+}
