08ae57032f8607ca49ba389a70af2523e644f7ab
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-10-30 12:12:20 +0300

    30.10.23 HW_11

--- src/com/urise/webapp/util/DeadLock.java
+++ src/com/urise/webapp/util/DeadLock.java
@@ -6,19 +6,19 @@ public class DeadLock {
 
     public static void main(String[] args) {
         new Thread(() -> deadLockRun(lockFirst, lockSecond)).start();
-        new Thread(() -> deadLockRun(lockSecond,lockFirst)).start();
+        new Thread(() -> deadLockRun(lockSecond, lockFirst)).start();
     }
 
     private static void deadLockRun(Object first, Object second) {
         synchronized (first) {
-            System.out.println("Поток удерживает блокировку 1");
+            System.out.println("Поток " + Thread.currentThread().getName() + " удерживает блокировку 1");
             try {
                 Thread.sleep(10);
             } catch (InterruptedException ignored) {
             }
-            System.out.println("Поток ожидает блокировку 2");
+            System.out.println("Поток " + Thread.currentThread().getName() + " ожидает блокировку 2");
             synchronized (second) {
-                System.out.println("Поток удерживает блокировку 1 и 2");
+                System.out.println("Поток " + Thread.currentThread().getName() + " удерживает блокировку 1 и 2");
             }
         }
     }
