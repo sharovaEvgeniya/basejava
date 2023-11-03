f6fe6793627d0e28f4f3afc94b6eda14481a81d1
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-11-03 17:50:41 +0300

    3.11.23 HW_12

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -1,18 +1,20 @@
 package com.urise.webapp;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class MainConcurrency {
-    private static int counter;
-    private static final int THREADS_NUMBER = 10000;
-    private static final Lock lock = new ReentrantLock();
-    private static final AtomicInteger atomicInteger = new AtomicInteger();
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    public static final int THREADS_NUMBER = 10000;
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = ThreadLocal.withInitial(() -> new SimpleDateFormat("dd.MM.yyyy HH:mm:ss"));
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
+
         Thread thread0 = new Thread() {
             @Override
             public void run() {
@@ -21,16 +23,7 @@ public class MainConcurrency {
         };
         thread0.start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(Thread.currentThread().getName()
-                        + ", " + Thread.currentThread().getState());
-            }
-        }).start();
-
-        new Thread(() -> System.out.println(Thread.currentThread().getName()
-                + ", " + Thread.currentThread().getState())).start();
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState())).start();
 
         System.out.println(thread0.getState());
 
@@ -39,23 +32,44 @@ public class MainConcurrency {
         ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            Future<Integer> future = executorService.submit(() -> {
+
+            Future<Integer> future = executorService.submit(() ->
+            {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
+                    System.out.println(threadLocal.get().format(new Date()));
                 }
                 latch.countDown();
-                return counter;
+                return 5;
             });
         }
 
         latch.await(10, TimeUnit.SECONDS);
         executorService.shutdown();
-        System.out.println(atomicInteger.get());
 
-        new MainConcurrency().inc();
+        System.out.println(mainConcurrency.atomicCounter.get());
+
+    }
+
+    private static void deadLock(Object lock1, Object lock2) {
+        new Thread(() -> {
+            System.out.println("Waiting " + lock1);
+            synchronized (lock1) {
+                System.out.println("Holding " + lock1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Waiting " + lock2);
+                synchronized (lock2) {
+                    System.out.println("Holding " + lock2);
+                }
+            }
+        }).start();
     }
 
     private void inc() {
-      atomicInteger.incrementAndGet();
+        atomicCounter.incrementAndGet();
     }
 }
--- src/com/urise/webapp/MainStreams.java
+++ src/com/urise/webapp/MainStreams.java
@@ -2,13 +2,18 @@ package com.urise.webapp;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainStreams {
     public static void main(String[] args) {
-        int[] itemsFirst = new int[]{1,2,3,3,2,3};
-        int[] itemsSecond = new int[]{9,8};
+        int[] itemsFirst = new int[]{1, 2, 3, 3, 2, 3};
+        int[] itemsSecond = new int[]{9, 8};
+        List<Integer> integerListFirst = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> integerListSecond = Arrays.asList(2, 2, 4, 6, 1, 3, 8);
         System.out.println(minValue(itemsFirst));
         System.out.println(minValue(itemsSecond));
+        System.out.println(oddOrEven(integerListFirst));
+        System.out.println(oddOrEven(integerListSecond));
     }
 
     private static int minValue(int[] values) {
@@ -16,7 +21,9 @@ public class MainStreams {
                 .distinct()
                 .sorted().reduce(0, (a, b) -> 10 * a + b);
     }
-    private static List<Integer> oddOrEven(List<Integer> integers) {
 
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        int del = integers.stream().mapToInt(Integer::intValue).sum() % 2;
+        return integers.stream().filter(elem -> elem % 2 != del).collect(Collectors.toList());
     }
 }
